fa1d4afc2da6ea4394027509004d8b7cbe251645
Adam Bender <abender@greenenergycorp.com>
2011-03-18 14:33:34 -0600

    initial import

---
+++ src/main/java/com/greenenergycorp/gwttemplate/client/gwttemplate.java
@@ -0,0 +1,19 @@
+package com.greenenergycorp.gwttemplate.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class gwttemplate implements EntryPoint
+{
+
+    @Override
+    public void onModuleLoad()
+    {
+        RootPanel.get().add(new Label("Hello World"));
+    }
+
+}
