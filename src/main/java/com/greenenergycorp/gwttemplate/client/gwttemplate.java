c71cfbb2ea7f32bef801de7046d42bc782b57897
Adam Bender <abender@greenenergycorp.com>
2011-10-20 10:19:30 -0600

    updated to use hijacked multiwordsuggestion

--- src/main/java/com/greenenergycorp/gwttemplate/client/gwttemplate.java
+++ src/main/java/com/greenenergycorp/gwttemplate/client/gwttemplate.java
@@ -1,18 +1,17 @@
 package com.greenenergycorp.gwttemplate.client;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.google.common.collect.Lists;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SuggestBox;
-import com.google.gwt.user.client.ui.SuggestOracle;
+
 import com.greenenergycorp.gwttemplate.client.gwtp.MyGinjector;
-import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServer;
-import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServerResult;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -20,6 +19,9 @@ import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServerResult;
 public class gwttemplate implements EntryPoint
 {
     public static final MyGinjector ginjector = GWT.create( MyGinjector.class );
+    private static final Logger LOGGER = Logger.getLogger( gwttemplate.class.getName() );
+    private static Label endlabel = new Label();
+    private static Label debuglabel = new Label();
 
     @Override
     public void onModuleLoad()
@@ -28,46 +30,179 @@ public class gwttemplate implements EntryPoint
         CustomOracle oracle = new CustomOracle();
         SuggestBox box = new SuggestBox( oracle );
         RootPanel.get().add( box );
+        RootPanel.get().add( endlabel );
+        RootPanel.get().add( debuglabel );
     }
 
-    private static class CustomOracle extends SuggestOracle
+    private static List<String> fields = Lists.newArrayList( "user:", "type:" );
+    private static List<String> users = Lists.newArrayList( "adam", "adrian", "chuck", "charles", "dan", "dennis" );
+    private static List<String> types = Lists.newArrayList( "open", "operational", "closed", "canceled", "error" );
+
+    private class CustomOracle extends MultiWordSuggestOracle
     {
         @Override
         public void requestSuggestions( final Request request, final Callback callback )
         {
-            ginjector.getDispatchAsync().execute( new SendTextToServer( request.getQuery() ), new AsyncCallback<SendTextToServerResult>() {
-
-                @Override
-                public void onFailure( Throwable caught )
-                {
-                }
-
-                @Override
-                public void onSuccess( SendTextToServerResult result )
-                {
-                    List<Suggestion> suggestions = Lists.newArrayList();
-                    for ( final String r : result.getResponses() )
-                    {
-                        suggestions.add( new Suggestion() {
-
-                            @Override
-                            public String getReplacementString()
-                            {
-                                return r + " replaced";
-                            }
-
-                            @Override
-                            public String getDisplayString()
-                            {
-                                return r + " display";
-                            }
-                        } );
-                    }
-                    callback.onSuggestionsReady( request, new Response( suggestions ) );
-                }
-            } );
-
+            // LOGGER.info( request.getQuery() );
+            Result r = AwesomeSuggestionStateMachine.computeState( request.getQuery() );
+            clear();
+            if ( r.finalState == AwesomeSuggestionStateMachine.State.FIELD || r.finalState == AwesomeSuggestionStateMachine.State.NEXT_TOKEN )
+            {
+                addAll( fields );
+                request.setQuery( r.tokens.get( r.tokens.size() - 1 ).field );
+                super.requestSuggestions( request, callback );
+            }
+            else if ( r.tokens.get( r.tokens.size() - 1 ).field.endsWith( "user" ) )
+            {
+                addAll( users );
+                request.setQuery( r.tokens.get( r.tokens.size() - 1 ).value );
+                super.requestSuggestions( request, callback );
+            }
+            else
+            {
+                addAll( types );
+                request.setQuery( r.tokens.get( r.tokens.size() - 1 ).value );
+                super.requestSuggestions( request, callback );
+            }
         }
     }
+
+    private static class AwesomeSuggestionStateMachine
+    {
+        enum State
+        {
+            NEXT_TOKEN
+            {
+                @Override
+                public State nextState( char c )
+                {
+                    switch ( c )
+                    {
+                        case ' ':
+                            return NEXT_TOKEN;
+                        case ':':
+                            return ERROR;
+                        default:
+                            return FIELD;
+                    }
+                }
+            },
+            FIELD
+            {
+                @Override
+                public State nextState( char c )
+                {
+                    switch ( c )
+                    {
+                        case ':':
+                            return VALUE;
+                        case ' ':
+                            return ERROR;
+                        default:
+                            return FIELD;
+                    }
+                }
+            },
+            VALUE
+            {
+                @Override
+                public State nextState( char c )
+                {
+                    switch ( c )
+                    {
+                        case ' ':
+                            return NEXT_TOKEN;
+                        case ':':
+                            return ERROR;
+                        default:
+                            return VALUE;
+                    }
+                }
+            },
+            ERROR
+            {
+                @Override
+                public State nextState( char c )
+                {
+                    return ERROR;
+                }
+            };
+
+            public abstract State nextState( char c );
+        }
+
+        public static Result computeState( String input )
+        {
+            State currentState = State.NEXT_TOKEN;
+            String progression = currentState.name();
+            int lastTokenStart = 0;
+            int currentIndex = 0;
+            List<Token> tokens = Lists.newArrayList();
+            for ( char c : input.toCharArray() )
+            {
+                currentState = currentState.nextState( c );
+                progression += ", " + currentState.name();
+                if ( currentState == State.ERROR )
+                {
+                    break;
+                }
+                else if ( currentState == State.NEXT_TOKEN )
+                {
+                    tokens.add( parseToken( input.substring( lastTokenStart, currentIndex ).trim() ) );
+                    lastTokenStart = currentIndex;
+                }
+                currentIndex++;
+            }
+            tokens.add( parseToken( input.substring( lastTokenStart, currentIndex ).trim() ) );
+            debuglabel.setText( "State Progression: " + progression );
+            return new Result( currentState, tokens );
+        }
+        // based on token input go to next state, state determines available suggestsions.
+    }
+
+    private static Token parseToken( String unparsed )
+    {
+        String[] splitToken = unparsed.split( ":" );
+        String field = splitToken[0];
+        String value = splitToken.length == 2 ? splitToken[1] : "";
+        return new Token( field, value );
+    }
+
+    private static class Result
+    {
+        private AwesomeSuggestionStateMachine.State finalState;
+        private final List<Token> tokens;
+
+        public Result( AwesomeSuggestionStateMachine.State currentState, List<Token> tokens )
+        {
+            finalState = currentState;
+            this.tokens = tokens;
+        }
+
+        @Override
+        public String toString()
+        {
+            return "state: " + finalState.name() + " tokens: " + tokens;
+        }
+    }
+
+    private static class Token
+    {
+        private String field;
+        private String value;
+
+        public Token( String field, String value )
+        {
+            this.field = field;
+            this.value = value;
+        }
+
+    }
+    // two phase
+    // build suggestion list
+    // ) using trie for prefix search, state machine to determine next set of suggestions
+    // parse query into reef query
+    // ) state machines to parse query into tokens useful in querying reef
+
 
 }
--- src/main/java/com/greenenergycorp/gwttemplate/server/gwtp/handler/SendTextToServerHandler.java
+++ src/main/java/com/greenenergycorp/gwttemplate/server/gwtp/handler/SendTextToServerHandler.java
@@ -1,6 +1,6 @@
 package com.greenenergycorp.gwttemplate.server.gwtp.handler;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -8,18 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServer;
-import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServerResult;
 import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.server.actionhandler.ActionHandler;
 import com.gwtplatform.dispatch.shared.ActionException;
 
+import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServer;
+import com.greenenergycorp.gwttemplate.shared.gwtp.SendTextToServerResult;
+
 public class SendTextToServerHandler implements ActionHandler<SendTextToServer, SendTextToServerResult>
 {
-
     private Provider<HttpServletRequest> requestProvider;
     private ServletContext servletContext;
-    private ArrayList<String> theList = Lists.newArrayList( "apple", "banana", "kiwi", "taco", "table", "kwikset", "beach", "ape" );
+    private List<String> theList = Lists.newArrayList( "apple", "banana", "kiwi", "taco", "table", "kwikset", "beach", "ape" );
 
     @Inject
     SendTextToServerHandler( ServletContext servletContext, Provider<HttpServletRequest> requestProvider )
@@ -31,12 +31,14 @@ public class SendTextToServerHandler implements ActionHandler<SendTextToServer, 
     @Override
     public SendTextToServerResult execute( SendTextToServer action, ExecutionContext context ) throws ActionException
     {
-        ArrayList<String> list = Lists.newArrayList();
+        List<String> list = Lists.newArrayList();
         String query = action.getTextToServer();
         for ( String entry : theList )
         {
             if ( entry.contains( query ) )
+            {
                 list.add( entry );
+            }
         }
         return new SendTextToServerResult( list );
     }
